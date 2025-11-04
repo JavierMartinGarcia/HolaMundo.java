a621f5dd06104c0530567a6915fd71e78947c4a0
JavierMartinGarcia <jmargar295w@g.educaand.es>
2025-11-04 10:21:19 +0100

    V2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+    static Scanner sc= new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num1,num2;
+        try{
+            System.out.println("Hello, World!");
+            System.out.print("Escriba dos números separados por un espacio: ");
+            num1= sc.nextInt();
+            num2= sc.nextInt();
+            int res=num1/num2;
+        System.out.printf("El resultado de la división es: %d \n",res);
+        }catch(InputMismatchException e){
+            System.err.println("se tiene que introducir números, no letras");
+        }        
     }
 }
